bc488ed38a01286bf3fa78795a96ad654f2f8737
MDCI <pedro.carpio@scytl.com>
2014-11-24 18:15:18 -0500

    cambio 12

--- src/main/java/pe/scytl/app/service/impl/TerritoryService.java
+++ src/main/java/pe/scytl/app/service/impl/TerritoryService.java
@@ -1,6 +1,6 @@
 package pe.scytl.app.service.impl;
 
-import pe.scytl.app.dao.jdbc.TerritoryDao;
+import pe.scytl.app.dao.GenericDao;
 import pe.scytl.app.entity.Territory;
 import pe.scytl.app.service.GenericService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +11,8 @@ import java.util.List;
 @Service
 public class TerritoryService implements GenericService<Territory> {
 
-    @Autowired
-    private TerritoryDao territoryDao;
+	@Autowired
+    private GenericDao<Territory> territoryDao;
 
     @Override
     public List<Territory> findAll() {
